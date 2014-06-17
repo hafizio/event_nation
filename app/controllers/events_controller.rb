8be091490a681b193c0af8000593f65c9522bd72
Hafiz <ibnukamy@gmail.com>
2014-06-17 16:44:35 +0800

    Initial commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,70 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: :index
+  before_action :event_owner!, only: [:edit, :update, :destroy]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = current_user.organized_events.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:title, :start_date, :end_date, :location, :agenda, :address, :organizer_id)
+    end
+
+    def event_owner!
+      authenticate_user!
+      if @event.organizer_id != current_user.id
+        redirect_to root_path
+        flash[:notice] = 'You do not have enough permission to do this'
+      end
+    end
+end
